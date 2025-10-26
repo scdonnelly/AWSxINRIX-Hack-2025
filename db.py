cf5dfad2ea9b83a67bdf67effecd5851bc5a1382
scdonnelly <scdonnelly@scu.edu>
2025-10-25 19:20:24 -0700

    Create db.py

---
+++ db.py
@@ -0,0 +1,73 @@
+class StudentData:
+    {
+        "company": 'AWS';
+        "FirstName": 'John';
+        "LastName": 'Doe';
+        "assignment": 
+
+    }
+    """Encapsulates an Amazon DynamoDB table of movie data.
+
+    Example data structure for a movie record in this table:
+        {
+            "year": 1999,
+            "title": "For Love of the Game",
+            "info": {
+                "directors": ["Sam Raimi"],
+                "release_date": "1999-09-15T00:00:00Z",
+                "rating": 6.3,
+                "plot": "A washed up pitcher flashes through his career.",
+                "rank": 4987,
+                "running_time_secs": 8220,
+                "actors": [
+                    "Kevin Costner",
+                    "Kelly Preston",
+                    "John C. Reilly"
+                ]
+            }
+        }
+    """
+
+    def __init__(self, dyn_resource):
+        """
+        :param dyn_resource: A Boto3 DynamoDB resource.
+        """
+        self.dyn_resource = dyn_resource
+        # The table variable is set during the scenario in the call to
+        # 'exists' if the table exists. Otherwise, it is set by 'create_table'.
+        self.table = None
+
+
+    def create_table(self, table_name):
+        """
+        Creates an Amazon DynamoDB table that can be used to store movie data.
+        The table uses the release year of the movie as the partition key and the
+        title as the sort key.
+
+        :param table_name: The name of the table to create.
+        :return: The newly created table.
+        """
+        try:
+            self.table = self.dyn_resource.create_table(
+                TableName=table_name,
+                KeySchema=[
+                    {"AttributeName": "year", "KeyType": "HASH"},  # Partition key
+                    {"AttributeName": "title", "KeyType": "RANGE"},  # Sort key
+                ],
+                AttributeDefinitions=[
+                    {"AttributeName": "year", "AttributeType": "N"},
+                    {"AttributeName": "title", "AttributeType": "S"},
+                ],
+                BillingMode='PAY_PER_REQUEST',
+            )
+            self.table.wait_until_exists()
+        except ClientError as err:
+            logger.error(
+                "Couldn't create table %s. Here's why: %s: %s",
+                table_name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return self.table
