bfe7545b3cd48c414ec0b14b7419815a6e67b3fc
fogunfunmi <fogunfunmi@scu.edu>
2025-10-25 19:23:01 -0700

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,63 @@
+
+
+class Movies:
+    """Encapsulates an Amazon DynamoDB table of movie data.
+
+    Example data structure for a movie record in this table:
+        {
+            "year": 1999,
+            "title": "For Love of the Game",
+            "info": {
+                "directors": ["Sam Raimi"],
+                "release_date": "1999-09-15T00:00:00Z",
+                "rating": 6.3,
+                "plot": "A washed up pitcher flashes through his career.",
+                "rank": 4987,
+                "running_time_secs": 8220,
+                "actors": [
+                    "Kevin Costner",
+                    "Kelly Preston",
+                    "John C. Reilly"
+                ]
+            }
+        }
+    """
+
+    def __init__(self, dyn_resource):
+        """
+        :param dyn_resource: A Boto3 DynamoDB resource.
+        """
+        self.dyn_resource = dyn_resource
+        # The table variable is set during the scenario in the call to
+        # 'exists' if the table exists. Otherwise, it is set by 'create_table'.
+        self.table = None
+
+
+    def add_movie(self, title, year, plot, rating):
+        """
+        Adds a movie to the table.
+
+        :param title: The title of the movie.
+        :param year: The release year of the movie.
+        :param plot: The plot summary of the movie.
+        :param rating: The quality rating of the movie.
+        """
+        try:
+            self.table.put_item(
+                Item={
+                    "year": year,
+                    "title": title,
+                    "info": {"plot": plot, "rating": Decimal(str(rating))},
+                }
+            )
+        except ClientError as err:
+            logger.error(
+                "Couldn't add movie %s to table %s. Here's why: %s: %s",
+                title,
+                self.table.name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+
